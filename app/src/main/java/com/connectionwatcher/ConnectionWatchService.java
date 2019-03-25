63fbeada8eddb883ebd0b8461c68364888fb2ea9
dhamaoka <dhamaoka@gmail.com>
2019-03-26 00:11:33 +0900

    初版

---
+++ app/src/main/java/com/connectionwatcher/ConnectionWatchService.java
@@ -0,0 +1,166 @@
+package com.connectionwatcher;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class ConnectionWatchService extends Service {
+    private final static String TAG = "ConnectionWatchService";
+    private Timer timer = new Timer();
+    private BluetoothReceiver bluetoothReceiver = new BluetoothReceiver();
+    private boolean IsBluetoothConnected = true;
+    int INTERVAL_PERIOD = 20000;
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand");
+        final IntentFilter bluetoothFilter = new IntentFilter();
+        bluetoothFilter.addAction(BluetoothDevice.ACTION_ACL_CONNECTED);
+        bluetoothFilter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED);
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                Log.d(TAG, "Working...");
+
+                BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+                if (bluetoothAdapter != null) {
+                    Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+                    if (pairedDevices.size() > 0) {
+                        for (BluetoothDevice device : pairedDevices) {
+                            if (device.getBondState() == BluetoothDevice.BOND_BONDED) {
+                                registerReceiver(bluetoothReceiver, bluetoothFilter);
+
+                                if (!IsBluetoothConnected) {
+                                    showNotification();
+                                }
+                                break;
+                            }
+                        }
+                    }
+                } else {
+                    stopSelf();
+                }
+            }
+        }, 0, INTERVAL_PERIOD);
+        return START_STICKY;
+    }
+
+    private class BluetoothReceiver extends BroadcastReceiver {
+        public void onReceive(Context arg0, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action)) {
+                IsBluetoothConnected = false;
+            } else if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)) {
+                String displayChar = getResources().getString(R.string.Connected);
+                Toast toast = Toast.makeText(getApplicationContext(), displayChar, Toast.LENGTH_SHORT);
+                toast.show();
+                IsBluetoothConnected = true;
+            }
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (timer != null) {
+            timer.cancel();
+        }
+        unregisterReceiver(bluetoothReceiver);
+        Log.d(TAG, "onDestroy");
+    }
+
+    /*
+     * MainActivitiy.xmlから移動。
+     */
+    public void showNotification() {
+
+        // 適当にサンプルからだけど、ホントに好きな数字でええもんなんかね。
+        // アプリ内でのユニーク？複数通知使わんかったらホンマになんでも良し？
+        int NOTIFICATION_ID = 1020;
+
+        NotificationManager notificationManager = (NotificationManager)
+                this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        String CHANNEL_ID = "BT Connection Watcher";    //任意のIDらしい。
+        //任意の名前らしい。
+        CharSequence name = getResources().getString(R.string.NotifyDescription);
+        //必須じゃないらしい。NotifyDescription
+        String Description = getResources().getString(R.string.NotifyDescription);
+        //通知のタイトルだわな。
+        String title = getResources().getString(R.string.app_name);
+        //通知のメッセージだわな。
+        String message = getResources().getString(R.string.ConnectionLost);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //デフォルトの重要度。Highっぽいぜ。
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            //チャンネル作るぜ。
+            NotificationChannel mChannel = new NotificationChannel(
+                    CHANNEL_ID, name, importance);
+            //説明セット。
+            mChannel.setDescription(Description);
+            //光るぜ。多分要らないぜ。だってスマートウォッチだし。
+            mChannel.enableLights(true);
+            //赤いぜ。多分要らないぜ。だってスマートウォッチだし。
+            mChannel.setLightColor(Color.RED);
+            //震えるぜヒート。
+            mChannel.enableVibration(true);
+            //バイブパターンだぜ。これも拾ったままでよく分かってないぜ。
+            mChannel.setVibrationPattern(new long[]{
+                    100, 200, 300, 400, 500, 400, 300, 200, 400});
+            //バッチはつけないぜ。
+            mChannel.setShowBadge(false);
+            //NotificationManagerのメソッドでCreateするぜ。
+            notificationManager.createNotificationChannel(mChannel);
+        }
+
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(
+                this,
+                0,
+                resultIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification.Builder builder = new Notification.Builder(this, CHANNEL_ID);
+        builder.setContentTitle(title);
+        builder.setSmallIcon(R.drawable.ic_stat_conn);
+        builder.setContentText(message);
+        builder.setAutoCancel(true);
+        builder.setContentIntent(resultPendingIntent);
+        builder.setWhen(System.currentTimeMillis());
+        Notification notification = builder.build();
+
+        //いよいよ通知するぜ！
+        notificationManager.notify(NOTIFICATION_ID, notification);
+    }
+}
---
+++ app/src/main/java/com/connectionwatcher/MainActivity.java
@@ -0,0 +1,110 @@
+package com.connectionwatcher;
+
+import android.app.ActivityManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class MainActivity extends WearableActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // idがswitchButtonのSwitchを取得
+        final Switch switchButton = findViewById(R.id.switch1);
+
+        //サービスが既に起動している場合は、Trueに。
+        if (isServiceWorking()) {
+            switchButton.setChecked(true);
+            switchButton.setText(getResources().getText(R.string.SwitchOn));
+        } else {
+            switchButton.setChecked(false);
+            switchButton.setText(getResources().getText(R.string.SwitchOff));
+        }
+
+        // switchButtonのオンオフが切り替わった時の処理を設定
+        switchButton.setOnCheckedChangeListener(
+                new CompoundButton.OnCheckedChangeListener() {
+                    public void onCheckedChanged(CompoundButton comButton, boolean isChecked) {
+                        // 表示する文字列をスイッチのオンオフで変える
+                        String displayChar;
+                        // オンなら
+                        if (isChecked) {
+                            displayChar = getResources().getString(R.string.SwitchOn);
+                            Intent sv = new Intent(getBaseContext(), ConnectionWatchService.class);
+                            startService(sv);
+                            switchButton.setText(getResources().getText(R.string.SwitchOn));
+                        }
+                        // オフなら
+                        else {
+                            displayChar = getResources().getString(R.string.SwitchOff);
+                            stopService(new Intent(getBaseContext(), ConnectionWatchService.class));
+                            switchButton.setText(getResources().getText(R.string.SwitchOff));
+                        }
+                        Toast toast = Toast.makeText(MainActivity.this, displayChar, Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                }
+        );
+
+        // Enables Always-on
+        setAmbientEnabled();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // idがswitchButtonのSwitchを取得
+        final Switch switchButton = findViewById(R.id.switch1);
+
+        //サービスが既に起動している場合は、Trueに。
+        if (isServiceWorking()) {
+            switchButton.setChecked(true);
+            switchButton.setText(getResources().getText(R.string.SwitchOn));
+        } else {
+            switchButton.setChecked(false);
+            switchButton.setText(getResources().getText(R.string.SwitchOff));
+        }
+/*
+        // switchButtonのオンオフが切り替わった時の処理を設定
+        switchButton.setOnCheckedChangeListener(
+                new CompoundButton.OnCheckedChangeListener(){
+                    public void onCheckedChanged(CompoundButton comButton, boolean isChecked){
+                        // 表示する文字列をスイッチのオンオフで変える
+                        String displayChar;
+                        // オンなら
+                        if(isChecked){
+                            displayChar = getResources().getString(R.string.SwitchOn);
+                            Intent sv = new Intent(getApplicationContext(),ConnectionWatchService.class);
+                            startService(sv);
+                            switchButton.setText(getResources().getText(R.string.SwitchOn));
+                        }
+                        // オフなら
+                        else{
+                            displayChar = getResources().getString(R.string.SwitchOff);
+                            stopService(new Intent(getApplicationContext(),ConnectionWatchService.class));
+                            switchButton.setText(getResources().getText(R.string.SwitchOff));
+                        }
+                        Toast toast = Toast.makeText(MainActivity.this, displayChar, Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                }
+        );
+*/
+    }
+
+    //サービスが起動してるかの確認。
+    private boolean isServiceWorking() {
+        ActivityManager manager = (ActivityManager) this.getSystemService(ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo serviceInfo : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (ConnectionWatchService.class.getName().equals(serviceInfo.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
